6ddcf912cc7eac8032d012c0dcfceb7a0fac1f1e
gmazinter <gmazinter@gmail.com>
2020-06-23 11:05:25 +0300

    implemented basic animation

--- index.js
+++ index.js
@@ -2,20 +2,60 @@ const VideoSlide = function () {
 	const swipeRight = () => {
 		console.log('swiping right');
 		const current = document.querySelector('.video-wrapper.current');
-		let next;
-		const currentIsLast =
-			current.parentElement.lastElementChild === current;
-		if (currentIsLast) {
-			next = current.parentElement.querySelector('.video-wrapper');
-		} else {
-			next = current.nextElementSibling;
+		const parent = current.parentElement;
+		const animated =
+			parent.querySelector('.video-wrapper.animated-right') ||
+			parent.querySelector('.video-wrapper.animated-left');
+		if (animated) {
+			animated.className = animated.className.replace(
+				'animated-right',
+				''
+			);
+			animated.className = animated.className.replace(
+				'animated-left',
+				''
+			);
 		}
-		current.className = current.className.replace('current', '');
+		const currentIsLast = parent.lastElementChild === current;
+		let next;
+		next = currentIsLast
+			? parent.querySelector('.video-wrapper')
+			: current.nextElementSibling;
+		current.className = current.className.replace(
+			'current',
+			'animated-right'
+		);
 		next.className = `${next.className} current`;
 	};
 
 	const swipeLeft = () => {
 		console.log('swiping left');
+		const current = document.querySelector('.video-wrapper.current');
+		const parent = current.parentElement;
+		const animated =
+			parent.querySelector('.video-wrapper.animated-right') ||
+			parent.querySelector('.video-wrapper.animated-left');
+		if (animated) {
+			animated.className = animated.className.replace(
+				'animated-right',
+				''
+			);
+			animated.className = animated.className.replace(
+				'animated-left',
+				''
+			);
+		}
+		let prev;
+		const currentIsFirst =
+			parent.querySelector('.video-wrapper') === current;
+		prev = currentIsFirst
+			? parent.lastElementChild
+			: current.previousElementSibling;
+		current.className = current.className.replace(
+			'current',
+			'animated-left'
+		);
+		prev.className = `${prev.className} current`;
 	};
 
 	return {
